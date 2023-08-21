234aff177cf2ef393b7645c2abfc45546154361e
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-21 18:29:44 +0300

    Define a slice of state for categories

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  categories: [],
+};
+
+const categories = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+
+  },
+});
+
+export default categories.reducer;
