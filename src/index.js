b4c4e7543ad4ca93a802727125a2648804df4d0a
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-21 18:53:40 +0300

    Remove Provider (not mentioned in project requirments)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import store from './redux/store';
+// import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
 
