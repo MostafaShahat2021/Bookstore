09fac71b0564a4aa5d9b7d0db46162fbb1904a79
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-22 20:41:08 +0300

    provide store to app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// import store from './redux/store';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
+import CategoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
-
+    books: booksReducer,
+    categorie: CategoriesReducer,
   },
 });
 
