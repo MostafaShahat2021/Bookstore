17d4e140eaff3af0f6e3ab726e98c834a39162da
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-25 15:48:52 +0300

    Update books & navbar with basic styling

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+// import { CircularProgressbar } from 'react-circular-progressbar';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchBooks, deleteBook } from '../redux/books/booksSlice';
 
@@ -12,19 +13,53 @@ function BookList() {
 
   return (
     <div>
-      <h2 className="add-book-title">Book List</h2>
-      <div>
+      {/* <h2 className="add-book-title">Book List</h2> */}
+      <div className="books-container">
         {isLoading && <h4 className="loading">Loading...</h4>}
         {error && <h4 className="loading">Failed to fetch books</h4>}
-        {books.length <= 0 && <h4 className="loading">Oops! There are currently no books on the list. Please check back later or add some books to the list.</h4>}
+        {books.length <= 0 && (
+          <h4 className="loading">
+            Oops! There are currently no books on the list. Please check back
+            later or add some books to the list.
+          </h4>
+        )}
         {books.map((book) => (
           <div className="add-book-container" key={book.item_id}>
             <div className="book-container">
-              <p className="book-title">{book.title}</p>
-              <p className="book-author">{book.author}</p>
-              <button type="button" onClick={() => dispatch(deleteBook(book.item_id))}>
-                Delete
-              </button>
+              <div className="book-data">
+                <p className="category">Computer Science</p>
+                <p className="book-title">{book.title}</p>
+                <p className="book-author">{book.author}</p>
+                <button type="button" className="btn btn-1">
+                  Comments
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-2"
+                  onClick={() => dispatch(deleteBook(book.item_id))}
+                >
+                  Remove
+                </button>
+                <button type="button" className="btn btn-3">
+                  Edit
+                </button>
+              </div>
+              <div className="progress-container">
+                <div className="percentage-circle">
+                  <div className="rotation">
+                    <span className="blue-circle" />
+                  </div>
+                  <div className="percentage-sign">
+                    <h3>69%</h3>
+                    <p>Completed</p>
+                  </div>
+                </div>
+              </div>
+              <div className="chapter-container">
+                <p className="current-chapter">current chapter</p>
+                <p className="chpter">chapter 17</p>
+                <button type="button" className="btn-prog">update progress</button>
+              </div>
             </div>
           </div>
         ))}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 
 function Navbar() {
   return (
     <nav>
-      <ul>
-        <li><h1>BookStore CMS</h1></li>
-        <li>
+      <ul className="navbar">
+        <li className="navbar-brand">
+          <h1>BookStore CMS</h1>
+        </li>
+        <li className="navbar-brand">
           <NavLink to="/">Home</NavLink>
         </li>
-        <li>
+        <li className="navbar-brand">
           <NavLink to="/categories">categories</NavLink>
         </li>
       </ul>
+      <div className="user-icon">
+        <FaUser color="blue" size={20} />
+      </div>
     </nav>
   );
 }
